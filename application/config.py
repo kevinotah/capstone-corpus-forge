d1546b787d3a0fa8bfaa46bcf117576ef2fa17f8
Kevin Otah <kevin.ogbusuo@epita.fr>
2026-05-23 20:42:40 +0200

    added config file

---
+++ application/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+SECRET_KEY = "maximum-effort"
+UPLOAD_FOLDER = BASE_DIR / "uploads"
+DATABASE_PATH = BASE_DIR / "data" / "metadata.sqlite"
